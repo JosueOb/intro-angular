852eae4568d7d83e74130b5520e28e72304352d3
JosueOb <josuericardocando@hotmail.com>
2021-08-05 18:56:00 -0500

    Uso de módulos de forma manual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
-import { HeroComponent } from './heros/hero/hero.component';
-import { ListComponent } from './heros/list/list.component';
+import { HerosModule } from './heros/heros.module';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CounterComponent,
-    HeroComponent,
-    ListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HerosModule, CounterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
