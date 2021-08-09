104bf988c3b24d75cd48f83f27109e5ee4b63b6f
JosueOb <josuericardocando@hotmail.com>
2021-08-09 11:40:16 -0500

    Formato al módulo DBZ y su componente

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,17 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
 
-
-
 @NgModule({
-  declarations: [
-    MainPageComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    MainPageComponent
-  ],
+  declarations: [MainPageComponent],
+  imports: [CommonModule],
+  exports: [MainPageComponent],
 })
-export class DbzModule { }
+export class DbzModule {}
