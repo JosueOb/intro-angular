d01c24f49696c0980fab3559bc925300e1467730
JosueOb <josuericardocando@hotmail.com>
2021-08-09 17:41:31 -0500

    Uso del atributo ngModel en los inputs de un formulario

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Character {
+  name: string;
+  power: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+  newCharacter: Character = {
+    name: 'Trucks',
+    power: 14000,
+  };
+
   add() {
-    console.log('Hey!!');
+    console.log(this.newCharacter);
   }
 }
